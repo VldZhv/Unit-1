51f514a290798bf38d60613bbbecc6c258b6a677
Vladimir Zakharov <vldzhv@gmail.com>
2023-09-01 13:00:33 +0300

    Add tests to testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,2 +1,43 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void amountIsLessThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+
+        int actual = service.remain(amount);
+
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void amountIsEqualToBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 1000;
+
+        int actual = service.remain(amount);
+
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void amountIsGreaterThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+        int expected = 800;
+
+        int actual = service.remain(amount);
+
+        org.testng.Assert.assertEquals(actual, expected);
+    }
 }
+
+
+
